7ca5803f881834435e59cf0a90d5fdce92e38248
SnehaSinha016 <sneha.sinha1610@gmail.com>
2026-07-03 16:20:29 +0530

    Add AI engine URL configuration

--- server/services/aiService.js
+++ server/services/aiService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const AI_URL="http://127.0.0.1:8000";
+const AI_URL="https://decisiongenome.onrender.com/";
 const checkHealth=async()=>{
     const response=await axios.get(`${AI_URL}/health`);
     return response.data;
